ec1c2bd6ade43952b399c5e9d5deef63e3614df3
sykq <genericmailacc@gmail.com>
2019-10-29 18:30:43 +0100

    init for jwt handling + formatting

--- src/main/java/org/psc/playground/controller/MiscController.java
+++ src/main/java/org/psc/playground/controller/MiscController.java
@@ -1,12 +1,9 @@
 package org.psc.playground.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.psc.playground.DefaultExceptionHandlerAutoConfiguration;
 import org.psc.playground.logic.MiscLogic;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collections;
 import java.util.Map;
@@ -19,7 +16,7 @@ public class MiscController {
     private final MiscLogic miscLogic;
 
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-    public Map<String, String> getMisc(){
+    public Map<String, String> getMisc() {
         return Collections.singletonMap("status", "OK");
     }
 
@@ -27,5 +24,12 @@ public class MiscController {
     public Map<String, String> getException() throws Exception {
         String status = miscLogic.getException();
         return Map.of("status", status);
+    }
+
+    @PostMapping(
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE, MediaType.TEXT_XML_VALUE},
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public Map<String, Object> postMisc(@RequestBody Map<String, Object> body) {
+        return body;
     }
 }
