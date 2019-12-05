e0cfba1dc6fc8835aa38ef8ffa5b5a4c7bea5680
sykq <genericmailacc@gmail.com>
2019-12-05 18:49:31 +0100

    swagger through springfox

--- src/main/java/org/psc/playground/PlaygroundWsWorkerApplication.java
+++ src/main/java/org/psc/playground/PlaygroundWsWorkerApplication.java
@@ -2,50 +2,12 @@ package org.psc.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class PlaygroundWsWorkerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlaygroundWsWorkerApplication.class, args);
     }
-
-    @Bean
-    public Docket docket() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                    .apis(RequestHandlerSelectors.any())
-                    .paths(PathSelectors.any())
-                .build()
-                .pathMapping("/")
-                .enableUrlTemplating(false);
-    }
-
-//    @Bean
-//    public UiConfiguration uiConfig() {
-//        return UiConfigurationBuilder.builder()
-//                .deepLinking(true)
-//                .displayOperationId(false)
-//                .defaultModelsExpandDepth(1)
-//                .defaultModelExpandDepth(1)
-//                .defaultModelRendering(ModelRendering.EXAMPLE)
-//                .displayRequestDuration(false)
-//                .docExpansion(DocExpansion.NONE)
-//                .filter(false)
-//                .maxDisplayedTags(null)
-//                .operationsSorter(OperationsSorter.ALPHA)
-//                .showExtensions(false)
-//                .tagsSorter(TagsSorter.ALPHA)
-//                .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
-//                .validatorUrl(null)
-//                .build();
-//    }
 
 }
--- src/main/java/org/psc/playground/configuration/ApplicationConfiguration.java
+++ src/main/java/org/psc/playground/configuration/ApplicationConfiguration.java
@@ -1,18 +1,39 @@
 package org.psc.playground.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+@EnableSwagger2
 public class ApplicationConfiguration implements WebMvcConfigurer {
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("playground")
+                .apiInfo(new ApiInfoBuilder().description("playgorund").version("1.0").build())
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .enableUrlTemplating(false);
+    }
+
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/");
-        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**")
+//                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+//        registry.addResourceHandler("/swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
     }
 
 }
