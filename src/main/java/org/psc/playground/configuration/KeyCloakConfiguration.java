2b0518ae8a963f22ecc834f2e5e85f750a758f11
sykq <genericmailacc@gmail.com>
2020-03-10 20:02:41 +0100

    keycloak

---
+++ src/main/java/org/psc/playground/configuration/KeyCloakConfiguration.java
@@ -0,0 +1,23 @@
+package org.psc.playground.configuration;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * {@link Configuration} for KeyCloak-Integration
+ */
+@Configuration
+public class KeyCloakConfiguration {
+
+    /**
+     * Bug?: https://stackoverflow.com/questions/57787768/issues-running-example-keycloak-spring-boot-app
+     *
+     * @return KeycloakSpringBootConfigResolver
+     */
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+
+}
