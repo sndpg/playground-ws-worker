449475c176f5c26ef47b983e8969b7aaa8df761f
sykq <genericmailacc@gmail.com>
2020-04-11 10:59:41 +0200

    disable sessionManagement and csrf

--- src/main/java/org/psc/playground/configuration/KeycloakSecurityConfiguration.java
+++ src/main/java/org/psc/playground/configuration/KeycloakSecurityConfiguration.java
@@ -15,6 +15,7 @@ import org.springframework.boot.web.servlet.ServletListenerRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
@@ -45,8 +46,9 @@ public class KeycloakSecurityConfiguration extends KeycloakWebSecurityConfigurer
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
-        http.authorizeRequests()
-                .anyRequest().permitAll();
+        http.sessionManagement(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeRequests(httpConfigurer -> httpConfigurer.anyRequest().permitAll());
     }
 
     @Bean
