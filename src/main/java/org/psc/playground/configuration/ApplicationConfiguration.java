4329c1910a3b77c2f648d191991f98e7167f912b
sykq <genericmailacc@gmail.com>
2020-06-27 14:27:14 +0200

    change servlet filter, security config

--- src/main/java/org/psc/playground/configuration/ApplicationConfiguration.java
+++ src/main/java/org/psc/playground/configuration/ApplicationConfiguration.java
@@ -3,7 +3,6 @@ package org.psc.playground.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import reactor.core.publisher.ReplayProcessor;
@@ -16,7 +15,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-@EnableWebMvc
 public class ApplicationConfiguration implements WebMvcConfigurer {
     @Bean
     public Docket docket() {
