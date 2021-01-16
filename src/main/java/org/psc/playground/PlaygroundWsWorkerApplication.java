330fc6fbacddcaac4356dc3fb88cf9eeb5e4f507
sykq <genericmailacc@gmail.com>
2021-01-16 13:02:11 +0100

    test the spring boot 2.4 config import feature (via property spring.config.import) in conjunction with activation of properties conditional on active profile(s)

--- src/main/java/org/psc/playground/PlaygroundWsWorkerApplication.java
+++ src/main/java/org/psc/playground/PlaygroundWsWorkerApplication.java
@@ -1,13 +1,26 @@
 package org.psc.playground;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
+@Slf4j
 @SpringBootApplication
 public class PlaygroundWsWorkerApplication {
 
+    @Value("${greeting-message}")
+    private String greeting;
+
     public static void main(String[] args) {
         SpringApplication.run(PlaygroundWsWorkerApplication.class, args);
+    }
+
+    @EventListener(ApplicationReadyEvent.class)
+    public void applicationStarted(){
+        log.info("{}", greeting);
     }
 
 }
